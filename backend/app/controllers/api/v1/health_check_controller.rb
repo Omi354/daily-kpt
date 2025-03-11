ff1d30af5c0a1cd2ecd941177b37f91ae1718e77
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-03-11 11:16:13 +0900

    lint: rubocopの設定を行い、実行しました

--- backend/app/controllers/api/v1/health_check_controller.rb
+++ backend/app/controllers/api/v1/health_check_controller.rb
@@ -1,5 +1,5 @@
- class Api::V1::HealthCheckController < ApplicationController
+class Api::V1::HealthCheckController < ApplicationController
   def show
-    render json: {message: 'Success Health Check'}, status: :ok
+    render json: { message: "Success Health Check" }, status: :ok
   end
- end
+end
