096971c48703b7a03249d83f255456d7ab14420f
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-03-11 10:56:33 +0900

    test: RSpecの設定を行い、ヘルスチェック用コントローラーのテストコードを作成しました

---
+++ backend/spec/requests/health_check_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::HealthCheck", type: :request do
+  describe "GET /api/v1/health_checks" do
+    subject { get api_v1_health_check_path }
+    it "ステータスコード200と成功した際のメッセージが返る" do
+      subject
+      message = JSON.parse(response.body)['message']
+      expect(response).to have_http_status(200)
+      expect(message).to eq('Success Health Check')
+
+    end
+  end
+end
