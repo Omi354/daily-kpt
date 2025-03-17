9f8f3e7b7defa0f43ea219821c128447e9986481
NaomichiIzumi <bell.tsaeb.shoji@gmail.com>
2025-03-17 23:24:17 +0900

    chore: トークン用の設定ファイルを修正しました

--- backend/app/controllers/concerns/devise_hack_fake_session.rb
+++ backend/app/controllers/concerns/devise_hack_fake_session.rb
@@ -8,17 +8,17 @@ module DeviseHackFakeSession
 
     def destroy
     end
+  end
 
-    included do
-      before_action :set_fake_session
+  included do
+    before_action :set_fake_session
 
-      private
+    private
 
-        def set_fake_session
-          if Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
-            request.env["rack.session"] ||= ::DeviseHackFakeSession::FakeSession.new
-          end
+      def set_fake_session
+        if Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
+          request.env["rack.session"] ||= ::DeviseHackFakeSession::FakeSession.new
         end
-    end
+      end
   end
 end
