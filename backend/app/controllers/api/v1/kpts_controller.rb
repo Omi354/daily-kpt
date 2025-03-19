a03b022c784b4ce92f13358fb994cb44ce92b836
NaomichiIzumi <bell.tsaeb.shoji@gmail.com>
2025-03-19 14:43:03 +0900

    feat: showアクションを定義し、手動テストを行いました

--- backend/app/controllers/api/v1/kpts_controller.rb
+++ backend/app/controllers/api/v1/kpts_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::KptsController < Api::V1::BaseController
   before_action :authenticate_user!
 
+  def show
+    kpt = current_user.kpts.find_by(date: params[:date])
+    if kpt.present?
+      render json: { message: "Success", kpt: kpt }
+    else
+      render json: { message: "Not Found" }, status: :not_found
+    end
+  end
+
   def create
     kpt = current_user.kpts.new(kpt_params)
     if kpt.save
